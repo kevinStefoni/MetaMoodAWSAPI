3e3b3955d8d81266e2b9a41f88df4d5e786dda82
Kevin <kevinStefoni@gmail.com>
2023-03-13 12:17:18 -0500

    Created AutoMapper profile.

---
+++ MetaMoodAWSAPI/DTOs/SpotifyTrackDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace MetaMoodAWSAPI.DTOs
+{
+    public class SpotifyTrackDTO
+    {
+
+        public string? Name { get; set; }
+
+        public string? ReleaseDate { get; set; }
+
+        public int? Popularity { get; set; }
+
+        public double? Acousticness { get; set; }
+
+        public double? Danceability { get; set; }
+
+        public double? Energy { get; set; }
+
+        public double? Liveness { get; set; }
+
+        public double? Loudness { get; set; }
+
+        public double? Speechiness { get; set; }
+
+        public double? Tempo { get; set; }
+
+        public double? Instrumentalness { get; set; }
+
+        public double? Valence { get; set; }
+
+    }
+}
---
+++ MetaMoodAWSAPI/Services/AllMappersProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MetaMoodAWSAPI.DTOs;
+using MetaMoodAWSAPI.Entities;
+
+namespace MetaMoodAWSAPI.Services
+{
+    internal class AllMappersProfile : Profile
+    {
+
+        public AllMappersProfile() {
+
+            CreateMap<SpotifyTrack, SpotifyTrackDTO>();
+            CreateMap<SpotifyTrackDTO, SpotifyTrack>().ForMember(t => t.TrackId, opt => opt.Ignore())
+                                                      .ForMember(t => t.AlbumId, opt => opt.Ignore())
+                                                      .ForMember(t => t.Name, opt => opt.MapFrom(src => src.Name))
+                                                      .ForMember(t => t.TrackHref, opt => opt.Ignore())
+                                                      .ForMember(t => t.CoverImageUrl, opt => opt.Ignore())
+                                                      .ForMember(t => t.ReleaseDate, opt => opt.MapFrom(src => src.ReleaseDate))
+                                                      .ForMember(t => t.PreviewUrl, opt => opt.Ignore())
+                                                      .ForMember(t => t.Popularity, opt => opt.MapFrom(src => src.Popularity))
+                                                      .ForMember(t => t.Acousticness, opt => opt.MapFrom(src => src.Acousticness))
+                                                      .ForMember(t => t.Danceability, opt => opt.MapFrom(src => src.Danceability))
+                                                      .ForMember(t => t.Energy, opt => opt.MapFrom(src => src.Energy))
+                                                      .ForMember(t => t.Liveness, opt => opt.MapFrom(src => src.Liveness))
+                                                      .ForMember(t => t.Loudness, opt => opt.MapFrom(src => src.Loudness))
+                                                      .ForMember(t => t.Speechiness, opt => opt.MapFrom(src => src.Speechiness))
+                                                      .ForMember(t => t.Tempo, opt => opt.MapFrom(src => src.Tempo))
+                                                      .ForMember(t => t.Instrumentalness, opt => opt.MapFrom(src => src.Instrumentalness))
+                                                      .ForMember(t => t.Valence, opt => opt.MapFrom(src => src.Valence));
+
+
+        }
+    }
+}
