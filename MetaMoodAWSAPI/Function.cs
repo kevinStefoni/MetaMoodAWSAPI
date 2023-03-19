57588676c22c5bf207c8a4fba6041b68117e9dd7
Kevin <kevinStefoni@gmail.com>
2023-03-18 19:30:25 -0500

    Continued work on query parameters.

--- MetaMoodAWSAPI/Function.cs
+++ MetaMoodAWSAPI/Function.cs
@@ -2,13 +2,12 @@ using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using MetaMoodAWSAPI.DTOs;
 using MetaMoodAWSAPI.Entities;
+using MetaMoodAWSAPI.QueryParameterModels;
 using MetaMoodAWSAPI.Services;
 using MetaMoodAWSAPI.Validation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
-using System.Net;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -41,12 +40,36 @@ public class Function
     /// <returns>A selected page of tracks from the spotify tracks table</returns>
     public async Task<APIGatewayHttpApiV2ProxyResponse> GetTrackPageAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
     {
-        string strPageSize = request.QueryStringParameters["pageSize"];
-        string strPageNumber = request.QueryStringParameters["pageNumber"];
+        IDictionary<string, string> searchCriteria = new Dictionary<string, string>();
+        string? lowerReleaseDate = null, upperReleaseDate = null;
+
+        if(!(request.QueryStringParameters.TryGetValue("pageSize", out string? strPageSize)
+            && request.QueryStringParameters.TryGetValue("pageNumber", out string? strPageNumber)))
+        {
+            return Response.BadRequest();
+        }
+
         if (!request.QueryStringParameters.TryGetValue("sortby", out string? sortBy))
         {
             sortBy = "name";
         }
+
+        if(request.QueryStringParameters.TryGetValue("name", out string? name))
+        {
+            searchCriteria["name"] = name;
+        }
+        
+        if(request.QueryStringParameters.TryGetValue("lowerReleaseDate", out lowerReleaseDate)
+            || request.QueryStringParameters.TryGetValue("upperReleaseDate", out upperReleaseDate))
+        {
+            lowerReleaseDate ??= "0000-00-00";
+            upperReleaseDate ??= "9999-99-99";
+
+            searchCriteria["lowerReleaseDate"] = lowerReleaseDate;
+            searchCriteria["upperReleaseDate"] = upperReleaseDate;
+
+        }
+        
 
         if (!int.TryParse(strPageSize, out int iPageSize)
             || !int.TryParse(strPageNumber, out int iPageNumber)
---
+++ MetaMoodAWSAPI/QueryParameterModels/SpotifyParameters.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MetaMoodAWSAPI.QueryParameterModels
+{
+    internal class SpotifyParameters
+    {
+        [Required]
+        public int PageSize { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public string? SortBy { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? LowerReleaseDate { get; set; }
+
+        public string? UpperReleaseDate { get; set; }
+
+        public int? LowerPopularity { get; set; }
+
+        public int? UpperPopularity { get; set; }
+
+        public double? LowerAcousticness { get; set; }
+
+        public double? UpperAcousticness { get; set; }
+
+        public double? LowerDanceability { get; set; }
+
+        public double? UpperDanceability { get; set; }
+
+        public double? LowerEnergy { get; set; }
+
+        public double? UpperEnergy { get; set; }
+
+        public double? LowerLiveness { get; set; }
+
+        public double? UpperLiveness { get; set; }
+
+        public double? LowerSpeechiness { get; set; }
+
+        public double? UpperSpeechiness { get; set; }
+
+        public double? LowerTempo { get; set; }
+
+        public double? UpperTempo { get; set; }
+
+        public double? LowerInstrumentalness { get; set; }
+
+        public double? UpperInstrumentalness { get; set; }
+
+        public double? LowerValence { get; set; }
+
+        public double? UpperValence { get; set; }
+
+    }
+}
